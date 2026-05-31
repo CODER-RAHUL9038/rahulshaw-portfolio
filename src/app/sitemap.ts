e242eba5878a792c7ffb19df9b9ad1dc7cb38758
Rahul Shaw <rahulshaw903866@gmail.com>
2026-06-01 00:43:08 +0530

    fix: format sitemap date for better GSC compatibility

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,7 +6,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString().split(".")[0] + "Z",
       changeFrequency: "monthly",
       priority: 1,
     },
